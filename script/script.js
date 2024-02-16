8bb3bd6ea56ab521a5bfbab448dbca5b0e46e83b
tofayeLL <ahmedtofayel132@gmail.com>
2024-02-16 19:31:10 +0600

    finished all the part and also added modal and responsive

--- script/script.js
+++ script/script.js
@@ -2,17 +2,17 @@
 
 // to get card c
 const cards = document.getElementsByClassName('card');
-console.log(cards);
+
 
 
 let count = 1;
+let totalPrice = 0;
 
-for(const card of cards){
-    card.addEventListener('click', function(){
+for (const card of cards) {
+    card.addEventListener('click', function () {
         const title = card.querySelector('h3').innerText;
-
         const price = card.querySelector('span').innerText;
-        const display =  count +'.' + title + ' ' + parseFloat(price);
+        const display = count + '.' + title + ':' + parseFloat(price);
 
         // display the title and price
         const displayContent = document.getElementById('display-container');
@@ -20,19 +20,68 @@ for(const card of cards){
         p.innerText = display;
         displayContent.appendChild(p);
 
+        // total price
+        totalPrice = totalPrice + parseFloat(price);
+        console.log(totalPrice);
+        setInnerTextValue('total-price', totalPrice);
+
 
         count++;
 
-        
+
     })
 }
 
 
-function setInnerTextValue(id,value){
+// cupon code and apply button
+const applyButton = getAnyElementById('apply-btn');
+applyButton.addEventListener('click', function () {
+    // get input field value and convert value
+    const inputField = getAnyElementById('input-field').value;
+    const inputConvert = inputField.split(' ').join('').toUpperCase();
+
+    if (totalPrice >= 200) {
+        if (inputConvert === 'SELL200') {
+            const discountPrice = totalPrice * 0.2;
+            setInnerTextValue('discount-price', discountPrice);
+            const finalTotal = totalPrice - discountPrice;
+            setInnerTextValue('final-total', finalTotal);
+            getAnyElementById('input-field').value = '';
+
+        }
+        else {
+            alert('incorrect cupon code');
+            getAnyElementById('input-field').value = '';
+        }
+    }
+    else {
+        alert('Get cupon offer you should buy at least 200 $');
+        getAnyElementById('input-field').value = '';
+    }
+})
+
+
+// reset all option
+const reset = getAnyElementById('reset');
+reset.addEventListener('click', function () {
+    const Price = getAnyElementById('total-price');
+    Price.innerText = '';
+    const displayContent = document.getElementById('display-container');
+    displayContent.innerText = '';
+    setInnerTextValue('discount-price', '');
+    setInnerTextValue('final-total', '');
+    getAnyElementById('input-field').value = '';
+})
+
+
+
+
+
+function setInnerTextValue(id, value) {
     document.getElementById(id).innerText = value;
 }
 
-function getElementById(id){
+function getAnyElementById(id) {
     const element = document.getElementById(id);
     return element;
 }
